420d40828cdcae07bc5bcb85dc46a89b18d65341
florence-468 <ajayiflorence93@gmail.com>
2023-11-25 07:45:06 -0800

    first assignment

---
+++ script.js
@@ -0,0 +1,56 @@
+const change = document.getElementById("image1");
+change.addEventListener("click", (event) => {
+    event.target.style.background = "red";
+    event.target.style.color = "white";
+    event.target.innerText = "yaay i've been clicked, and i'm 1"
+
+});
+const change2 = document.getElementById("image2");
+change2.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "white";
+    event.target.innerText = "yaay i've been clicked, and i'm 2"
+
+});
+const change3 = document.getElementById("image3");
+change3.addEventListener("click", (event) => {
+    event.target.style.background = "purple";
+    event.target.style.color = "white";
+    event.target.innerText = "yaay i've been clicked, and i'm 3"
+
+});
+const change4 = document.getElementById("image4");
+change4.addEventListener("click", (event) => {
+    event.target.style.background = "brown";
+    event.target.style.color = "white";
+    event.target.innerText = "yaay i've been clicked, and i'm 4"
+
+});
+const change5 = document.getElementById("image5");
+change5.addEventListener("click", (event) => {
+    event.target.style.background = "blue";
+    event.target.style.color = "white";
+    event.target.innerText = "yaay i've been clicked, and i'm 5"
+
+});
+const change6 = document.getElementById("image6");
+change6.addEventListener("click", (event) => {
+    event.target.style.background = "black";
+    event.target.style.color = "white";
+    event.target.innerText = "yaay i've been clicked, and i'm 6";
+
+});
+const change7 = document.getElementById("image7");
+change7.addEventListener("click", (event) => {
+    event.target.style.background = "coral";
+    event.target.style.color = "white";
+    event.target.innerText = "yaay i've been clicked, and i'm 7";
+
+});
+const change8 = document.getElementById("image8");
+change8.addEventListener("click", (event) => {
+    event.target.style.background = "burlywood";
+    event.target.style.color = "white";
+    event.target.innerText = "yaay i've been clicked, and i'm 8";
+
+});
